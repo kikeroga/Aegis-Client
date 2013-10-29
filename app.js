9aa40848c06f0d149f4169d71d6a046944935e15
Roga <roga7zl@gmail.com>
2013-10-30 01:53:02 +0900

    Modify Logic

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-// const DESTINATION = 'http://localhost:3000';
-const DESTINATION = 'http://aegis-server.herokuapp.com';
+const PORT = 4000;
+// const DESTINATION = 'http://localhost:3000'; 
+//const DESTINATION = 'http://aegis-server.herokuapp.com:3000'; 
 
 var express = require('express');
 var routes = require('./routes/main.js');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var io = require('socket.io-client');
+//var io = require('socket.io-client');
 
 var app = express();
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || PORT);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -21,25 +22,17 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 //----------------------------------------------------------------------//
 
 app.get('/', routes.index);
 
-var server = http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+var socketio = require('socket.io');
+var io = socketio.listen(server);
+server.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
-//var io = require('socket.io-client').listen(server);
-//var io = require('socket.io-client');
 
-// Recieve
-var socket = io.connect(DESTINATION);
-socket.on('connect', function() {
-    console.log('Connect to Server: ' + DESTINATION);
-
-    socket.on('update', function(value) {
-        console.log(DESTINATION + ': ' + value);
-    });
-});
--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,11 +1,16 @@
 $(function() {
-    var socket = io.connect('http://localhost:3000');
-    socket.on('valueChange', function (data) {
-        $('#device1').html(data);
+    // var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://aegis-server.herokuapp.com');
+    socket.on('connect', function(){
+        socket.on('update', function (value) {
+            console.log('Receive: ' + value);
+            $('#device1').html(value);
+        });
     });
 
     $('#button').on('click', function(){
         var value = $('#value').val()
-        socket.emit('valueUpdate', value);
+        socket.emit('change', value);
+        console.log('Send: ' + value);
     });
 });
